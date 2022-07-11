b81585b6da6067cb6286aa60af726265f9c7262a
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-07-10 23:43:45 -0300

    Fix paths

--- src/constants/wireframes.js
+++ src/constants/wireframes.js
@@ -1,58 +1,58 @@
 export const wireframes = [
     {
         name: 'Pantalla inicial',
-        path: '/app-documentation/assets/wireframes/PANTALLA INICIAL.png'
+        path: '/assets/wireframes/PANTALLA INICIAL.png'
     },
     {
         name: 'Pantalla de registro',
-        path: '/app-documentation/assets/wireframes/REGISTRARSE.png'
+        path: '/assets/wireframes/REGISTRARSE.png'
     },
     {
         name: 'Inicio de sesión',
-        path: '/app-documentation/assets/wireframes/INICIAR SESIÓN.png'
+        path: '/assets/wireframes/INICIAR SESIÓN.png'
     },
     {
         name: 'Pantalla de inicio',
-        path: '/app-documentation/assets/wireframes/HOME.png'
+        path: '/assets/wireframes/HOME.png'
     },
     {
         name: 'Pantalla de búsqueda',
-        path: '/app-documentation/assets/wireframes/SEARCH.png'
+        path: '/assets/wireframes/SEARCH.png'
     },
     {
         name: 'Resultado de búsqueda',
-        path: '/app-documentation/assets/wireframes/SEARCH RESULT.png'
+        path: '/assets/wireframes/SEARCH RESULT.png'
     },
     {
         name: 'Reproductor de música',
-        path: '/app-documentation/assets/wireframes/MUSIC PLAYER.png'
+        path: '/assets/wireframes/MUSIC PLAYER.png'
     },
     {
         name: 'Pantalla de feedback',
-        path: '/app-documentation/assets/wireframes/FEEDBACK.png'
+        path: '/assets/wireframes/FEEDBACK.png'
     },
     {
         name: 'Drawer principal',
-        path: '/app-documentation/assets/wireframes/HOME DRAWER.png'
+        path: '/assets/wireframes/HOME DRAWER.png'
     },
     {
         name: 'Pantalla de playlist',
-        path: '/app-documentation/assets/wireframes/PLAYLIST.png'
+        path: '/assets/wireframes/PLAYLIST.png'
     },
     {
         name: 'Pantalla de billetera',
-        path: '/app-documentation/assets/wireframes/WALLET.png'
+        path: '/assets/wireframes/WALLET.png'
     },
     {
         name: 'Extracción de fondos',
-        path: '/app-documentation/assets/wireframes/EXTRACTION.png'
+        path: '/assets/wireframes/EXTRACTION.png'
     },
     {
         name: 'Movimientos de la cuenta',
-        path: '/app-documentation/assets/wireframes/MOVEMENTS.png'
+        path: '/assets/wireframes/MOVEMENTS.png'
     },
     {
         name: 'Pantalla de suscripciones',
-        path: '/app-documentation/assets/wireframes/SUBSCRIPTIONS.png'
+        path: '/assets/wireframes/SUBSCRIPTIONS.png'
     },
 ]
